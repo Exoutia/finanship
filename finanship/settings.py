cb203042c4cb517ffa8ee830b7f2d4bb959b03bd
exoutia <jhav365@gmail.com>
2024-04-02 15:21:09 +0530

    registering the templates directory using os

--- finanship/settings.py
+++ finanship/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +56,7 @@ ROOT_URLCONF = 'finanship.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': [os.path.join(BASE_DIR,'accounts', 'templates')],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
