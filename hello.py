e9e3dc8a5aee8aad73d4230176bfc3b33203039c
exoutia <jhav365@gmail.com>
2025-02-21 08:29:07 +0530

    added the model in tracker application

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from finanship!")
-
-
-if __name__ == "__main__":
-    main()
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Record(models.Model):
+    TRANSACTION_TYPE_CHOICES = [('INCOME', "Income"), ('EXPENSE', 'Expense')]
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    transaction_type = models.CharField(max_length=8, choices=TRANSACTION_TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tags = models.ManyToManyField("Tag", related_name="tags")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
